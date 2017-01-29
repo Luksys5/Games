80f6c17c0d1b3f986594d0bc53043822fc152c16
Lukas Tutkus <Lukas Tutkus>
2017-01-29 21:55:55 +0200

    Creating new game guess number. Initialised start scene

---
+++ GuessNumber/Assets/Scripts/ToggleBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleBehaviour : MonoBehaviour {
+
+    public Toggle playerToogle;
+    public Toggle pcToogle;
+    // 1 - player, 0 - pc
+    public bool whoIsActive;
+
+
+    private void Start()
+    {
+        whoIsActive = playerToogle.isOn;
+    }
+
+    private void Update()
+    {
+        if(playerToogle.isOn == pcToogle.isOn)
+        {
+            whoIsActive = !whoIsActive;
+            playerToogle.isOn = whoIsActive;
+            pcToogle.isOn = !whoIsActive;
+            if(whoIsActive)
+            {
+                Debug.Log("Player Active");
+            }
+            else
+            {
+                Debug.Log("PC Active");
+            }
+        }
+    }
+
+}
