155d350990c244df10985691fff609106acb584f
Lukas Tutkus <Lukas Tutkus>
2017-02-04 22:16:10 +0200

    Adding Global data static class ant scene build indexes

---
+++ GuessNumber/Assets/Scripts/GlobalData.cs
@@ -0,0 +1,6 @@
+﻿public static class GlobalData {
+
+    public static int StartSceneBuildIndex = 0;
+    public static int PlayerGuessBuildIndex = 1;
+    public static int PCGUessBuildIndex = 2;
+}
